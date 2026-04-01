c806358914762bdcbe6edadc2a56bdddde9c29a4
Dev Agarwal <agarwaldev626@gmail.com>
2026-04-01 14:39:39 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:e_commerce_app/core/resources/fonts.dart';
 import 'package:e_commerce_app/core/services/injection_container.dart';
 import 'package:e_commerce_app/core/common/app/provider/local_provider.dart';
 import 'package:e_commerce_app/core/services/router.dart';
-import 'package:e_commerce_app/firebase_options.dart';
 import 'package:e_commerce_app/l10n/app_localizations.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -12,7 +11,7 @@ import 'package:provider/provider.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await init();
   runApp(
     MultiProvider(
